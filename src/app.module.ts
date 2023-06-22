b465e349398dd01916721919aea116072f25fa60
Ariel Arias <arielwwjd@gmail.com>
2023-06-22 18:00:08 -0400

    config module added to work with the .env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { StudentModule } from './student/student.module';
 import { CourseModule } from './course/course.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [StudentModule, CourseModule, PrismaModule],
+  imports: [
+    StudentModule,
+    CourseModule,
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
